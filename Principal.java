57f3a0fa393ffea7658090d60a405645b395cd6d
Rodger Gadelha <83789942+rodgergadelha@users.noreply.github.com>
2022-07-12 19:09:43 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -2,7 +2,7 @@ import view.MainFrame;
 
 public class Principal {
     /**
-     * @see <a href="https://jgraph.github.io/mxgraph/java/docs/index.html">Link para a documentação da biblioteca JGraphX</a>
+     * @see <a href="https://jgraph.github.io/mxgraph/java/docs/index.html">Link para a documentação da biblioteca JGraphX, usada nesse projeto.</a>
      */
     public static void main(String[] args) {
         new MainFrame();
