66f7cfa1809012edbe63355389595463a7bddd36
Rodger Gadelha <83789942+rodgergadelha@users.noreply.github.com>
2022-07-12 20:13:46 -0300

    Add files via upload

---
+++ view/Principal.java
@@ -0,0 +1,13 @@
+package view;
+
+/**
+* Classe com a função main.
+*/
+public class Principal {
+    /**
+     * @see <a href="https://jgraph.github.io/mxgraph/java/docs/index.html">Link para a documentação da biblioteca JGraphX, usada nesse projeto.</a>
+     */
+    public static void main(String[] args) {
+        new MainFrame();
+    }
+}
