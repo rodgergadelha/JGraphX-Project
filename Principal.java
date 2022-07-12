c771985b4fa0e98462e835eba25fffc023d3c046
Rodger Gadelha <83789942+rodgergadelha@users.noreply.github.com>
2022-07-12 19:08:50 -0300

    Add files via upload

--- Principal.java
+++ Principal.java
@@ -1,6 +1,9 @@
 import view.MainFrame;
 
 public class Principal {
+    /**
+     * @see <a href="https://jgraph.github.io/mxgraph/java/docs/index.html">Link para a documentação da biblioteca JGraphX</a>
+     */
     public static void main(String[] args) {
         new MainFrame();
     }
